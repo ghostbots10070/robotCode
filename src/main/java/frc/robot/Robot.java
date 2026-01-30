c6402f6b8b219bb0fe3f19a4214bc4e6267e6c69
meizuflux <76911441+meizuflux@users.noreply.github.com>
2026-01-30 16:19:10 -0500

    add visual detection on the apriltags

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,12 +7,18 @@
 
 package frc.robot;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.first.apriltag.AprilTagDetector;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.util.PixelFormat;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -35,44 +41,68 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
 
   private AprilTagDetector m_detector;
-  private Thread visionThread;
+  Thread visionThread;
 
   @Override
-public void robotInit() {
-    //Camera Stuff
-    CameraServer.startAutomaticCapture();
+  public void robotInit() {
+      m_detector = new AprilTagDetector();
+      m_detector.addFamily("tag36h11");
 
-    m_detector = new AprilTagDetector();
-    m_detector.addFamily("tag36h11");
+      visionThread = new Thread(
+        () -> {
+            UsbCamera camera = CameraServer.startAutomaticCapture();
+            //camera.setPixelFormat(PixelFormat.kYUYV);
+            //camera.setFPS(24); // http://roborio-10070-frc.local:1181/
+            camera.setResolution(320, 240);
 
-    visionThread = new Thread(() -> {
-          CvSink cvSink = CameraServer.getVideo();
-          Mat source = new Mat();
-          Mat grey = new Mat();
+            CvSink cvSink = CameraServer.getVideo();
 
-          while (!Thread.interrupted()) {
-            if (cvSink.grabFrame(source) == 0) continue; // Skip if no frame
+            CvSource outputStream = CameraServer.putVideo("Rectangle", 320, 240);
 
-            // Convert to grayscale for faster processing
-            Imgproc.cvtColor(source, grey, Imgproc.COLOR_BGR2GRAY);
+            Mat mat = new Mat();
+            Mat gray = new Mat();     // grayscale frame
 
-            // Run detection
-            var detections = m_detector.detect(grey);
-          
-            for (var detection : detections) {
-              // Access the ID of the tag (e.g., ID 1-22 for 2026)
-              int tagId = detection.getId();
-              System.out.println("TagID "+ tagId);
-              // Log or pass tagId to your RobotContainer/Drivetrain
+            while (!Thread.interrupted()) {
+              if (cvSink.grabFrame(mat) == 0) continue; // Skip if no frame
+
+              // april tag detector only works if grayscale imgae
+              Imgproc.cvtColor(mat, gray, Imgproc.COLOR_BGR2GRAY);
+              var detections = m_detector.detect(gray);
+            
+              for (var detection : detections) {
+                // Access the ID of the tag (e.g., ID 1-22 for 2026)
+                int tagId = detection.getId();
+
+                System.out.println("TagID "+ tagId);
+
+                double[] c = detection.getCorners();
+
+                Point p0 = new Point(c[0], c[1]);
+                Point p1 = new Point(c[2], c[3]);
+                Point p2 = new Point(c[4], c[5]);
+                Point p3 = new Point(c[6], c[7]);
+
+                Imgproc.line(mat, p0, p1, new Scalar(0, 0, 255), 2);
+                Imgproc.line(mat, p1, p2, new Scalar(0, 0, 255), 2);
+                Imgproc.line(mat, p2, p3, new Scalar(0, 0, 255), 2);
+                Imgproc.line(mat, p3, p0, new Scalar(0, 0, 255), 2);
+
+                //Imgproc.rectangle(grey, new Point(100, 100), new Point(150, 150), new Scalar(130, 130, 130), 5);
+                // Give the output stream a new image to display
+                // Log or pass tagId to your RobotContainer/Drivetrain
+              }
+              Core.flip(mat, mat, 1);
+              outputStream.putFrame(mat);
+
+              //source.release();
+              //grey.release();
             }
-              source.release();
-              grey.release();
-          }
-        });
-        visionThread.start();
+          });
+          visionThread.setDaemon(true);
+          visionThread.start();
 
-    m_robotContainer = new RobotContainer();
-  }
+      m_robotContainer = new RobotContainer();
+    }
 
 
   @Override
